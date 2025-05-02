4fb1aef788595bbc1639d26c56a1c591c9814b7d
Andre Schenato <andreschenato@gmail.com>
2025-05-02 15:46:49 -0300

    Added parallel processing with go routines

--- utils/process.go
+++ utils/process.go
@@ -4,40 +4,73 @@ import (
 	"image/color"
 	"image_processing/global"
 	"log/slog"
+	"runtime"
+	"sync"
+	"time"
 )
 
 type PixelTransformFunc func(color.RGBA) color.RGBA
 type PixelsTransformFunc func(color.RGBA, color.RGBA) color.RGBA
 type AxisTransformFunc func(x, y, width, height int, pixel color.RGBA) (int, int, color.RGBA)
 
-func single(fun PixelTransformFunc, width, height int, pixel, image [][]color.RGBA) [][]color.RGBA {
-	for x := range width {
-		for y := range height {
-			image[x][y] = fun(pixel[x][y])
+func parallelizeProcessing(width, height int, process func(startX, endX, height int)) {
+	start := time.Now()
+	numCPU := runtime.NumCPU()
+	var wg sync.WaitGroup
+	wg.Add(numCPU)
+
+	chunkSize := max(width/numCPU, 1)
+
+	for i := range numCPU {
+		startX := i * chunkSize
+		endX := (i + 1) * chunkSize
+		if i == numCPU-1 {
+			endX = width
 		}
+
+		go func(startX, endX int) {
+			defer wg.Done()
+			process(startX, endX, height)
+		}(startX, endX)
 	}
+	wg.Wait()
+	slog.Info(time.Since(start).String())
+}
+
+func single(fun PixelTransformFunc, width, height int, pixel, image [][]color.RGBA) [][]color.RGBA {
+	parallelizeProcessing(width, height, func(startX, endX, height int) {
+		for x := startX; x < endX; x++ {
+			for y := range height {
+				image[x][y] = fun(pixel[x][y])
+			}
+		}
+	})
 	return image
 }
 
 func both(fun PixelsTransformFunc, width, height int, pixelOne, pixelTwo, image [][]color.RGBA) [][]color.RGBA {
-	for x := range width {
-		for y := range height {
-			image[x][y] = fun(pixelOne[x][y], pixelTwo[x][y])
+	parallelizeProcessing(width, height, func(startX, endX, height int) {
+		for x := startX; x < endX; x++ {
+			for y := range height {
+				image[x][y] = fun(pixelOne[x][y], pixelTwo[x][y])
+			}
 		}
-	}
+	})
 	return image
 }
 
 func axis(fun AxisTransformFunc, width, height int, pixels, image [][]color.RGBA) [][]color.RGBA {
-	for x := range width {
-		for y := range height {
-			newX, newY, newColor := fun(x, y, width, height, pixels[x][y])
+	parallelizeProcessing(width, height, func(startX, endX, height int) {
+		for x := startX; x < endX; x++ {
+			for y := range height {
+				newX, newY, newColor := fun(x, y, width, height, pixels[x][y])
 
-			if newX >= 0 && newX < width && newY >= 0 && newY < height {
-				image[newX][newY] = newColor
+				if newX >= 0 && newX < width && newY >= 0 && newY < height {
+					image[newX][newY] = newColor
+				}
 			}
 		}
-	}
+	})
 	return image
 }
 
