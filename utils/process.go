704599cc0f007d9737b8f48a5fb1241450f5f78a
Andre Schenato <andreschenato@gmail.com>
2025-05-02 09:33:55 -0300

    Optimized process code to use single function

--- utils/process.go
+++ utils/process.go
@@ -10,119 +10,79 @@ type PixelTransformFunc func(color.RGBA) color.RGBA
 type PixelsTransformFunc func(color.RGBA, color.RGBA) color.RGBA
 type AxisTransformFunc func(x, y, width, height int, pixel color.RGBA) (int, int, color.RGBA)
 
-func single(fun PixelTransformFunc) {
-	img := *global.ImageOne
-	if img == nil {
-		img = *global.ImageTwo
-	}
-
-	pixels := ConvertImageToPixels(img)
-
-	xLen := len(pixels)
-	yLen := len(pixels[0])
-
-	newImage := make([][]color.RGBA, xLen)
-	for i := range newImage {
-		newImage[i] = make([]color.RGBA, yLen)
-	}
-
-	for x := range xLen {
-		for y := range yLen {
-			pixel := pixels[x][y]
-
-			newColor := fun(pixel)
-
-			newImage[x][y] = newColor
+func single(fun PixelTransformFunc, width, height int, pixel, image [][]color.RGBA) [][]color.RGBA {
+	for x := range width {
+		for y := range height {
+			image[x][y] = fun(pixel[x][y])
 		}
 	}
-
-	global.FinalImage.Image = ConvertPixelsToImage(newImage)
-	global.FinalImage.Refresh()
+	return image
 }
 
-func both(fun PixelsTransformFunc) {
-	pixelsOne := ConvertImageToPixels(*global.ImageOne)
-	pixelsTwo := ConvertImageToPixels(*global.ImageTwo)
-
-	xLen := len(pixelsOne)
-	yLen := len(pixelsOne[0])
-
-	newImage := make([][]color.RGBA, xLen)
-	for i := range xLen {
-		newImage[i] = make([]color.RGBA, yLen)
-	}
-
-	for x := range xLen {
-		for y := range yLen {
-			newColor := fun(pixelsOne[x][y], pixelsTwo[x][y])
-
-			newImage[x][y] = newColor
+func both(fun PixelsTransformFunc, width, height int, pixelOne, pixelTwo, image [][]color.RGBA) [][]color.RGBA {
+	for x := range width {
+		for y := range height {
+			image[x][y] = fun(pixelOne[x][y], pixelTwo[x][y])
 		}
 	}
-
-	global.FinalImage.Image = ConvertPixelsToImage(newImage)
-	global.FinalImage.Refresh()
+	return image
 }
 
-func axis(fun AxisTransformFunc) {
-	img := *global.ImageOne
-	if img == nil {
-		img = *global.ImageTwo
-	}
+func axis(fun AxisTransformFunc, width, height int, pixels, image [][]color.RGBA) [][]color.RGBA {
+	for x := range width {
+		for y := range height {
+			newX, newY, newColor := fun(x, y, width, height, pixels[x][y])
 
-	pixels := ConvertImageToPixels(img)
-
-	xLen := len(pixels)
-	yLen := len(pixels[0])
-
-	newImage := make([][]color.RGBA, xLen)
-	for i := range newImage {
-		newImage[i] = make([]color.RGBA, yLen)
-	}
-
-	for x := range xLen {
-		for y := range yLen {
-			pixel := pixels[x][y]
-
-			newX, newY, newColor := fun(x, y, xLen, yLen, pixel)
-
-			if newX >= 0 && newX < xLen && newY >= 0 && newY < yLen {
-				newImage[newX][newY] = newColor
+			if newX >= 0 && newX < width && newY >= 0 && newY < height {
+				image[newX][newY] = newColor
 			}
 		}
 	}
-
-	global.FinalImage.Image = ConvertPixelsToImage(newImage)
-	global.FinalImage.Refresh()
+	return image
 }
 
 func Process(service interface{}) func() {
 	return func() {
+		if *global.ImageOne == nil && *global.ImageTwo == nil {
+			slog.Error("No image set")
+			return
+		}
+
+		var pixelsTwo *[][]color.RGBA
+		imgOne := *global.ImageOne
+		if imgOne == nil {
+			imgOne = *global.ImageTwo
+		}
+
+		pixelsOne := ConvertImageToPixels(imgOne)
+		if *global.ImageTwo != nil {
+			p := ConvertImageToPixels(*global.ImageTwo)
+			pixelsTwo = &p
+		}
+
+		xLen := len(pixelsOne)
+		yLen := len(pixelsOne[0])
+
+		newImage := make([][]color.RGBA, xLen)
+		for i := range xLen {
+			newImage[i] = make([]color.RGBA, yLen)
+		}
+
 		switch s := service.(type) {
 		case PixelTransformFunc:
-			if *global.ImageOne != nil || *global.ImageTwo != nil {
-				single(s)
-				return
-			}
-			slog.Error("No image set")
-			return
+			newImage = single(s, xLen, yLen, pixelsOne, newImage)
 		case PixelsTransformFunc:
-			if *global.ImageOne != nil && *global.ImageTwo != nil {
-				both(s)
-				return
+			if pixelsTwo != nil {
+				newImage = both(s, xLen, yLen, pixelsOne, *pixelsTwo, newImage)
 			}
-			slog.Error("Images aren't set")
-			return
 		case AxisTransformFunc:
-			if *global.ImageOne != nil || *global.ImageTwo != nil {
-				axis(s)
-				return
-			}
-			slog.Error("No image set")
-			return
+			newImage = axis(s, xLen, yLen, pixelsOne, newImage)
 		default:
 			slog.Error("invalid service", "type", s)
 			return
 		}
+
+		global.FinalImage.Image = ConvertPixelsToImage(newImage)
+		global.FinalImage.Refresh()
 	}
 }
